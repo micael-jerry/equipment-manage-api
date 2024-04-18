3441bc3dd849b209dc10a2c2390bc4ca7c1fb8ba
Lakara2 <lbryanlakara@gmail.com>
2024-04-18 09:42:18 +0200

    update app.js

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const cors = require('cors');
 const routes = require('./routes/index');
 require('dotenv').config();
 
+// Connexion à MongoDB Atlas
 mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -15,22 +16,24 @@ db.once('open', () => {
   console.log('Connecté à la base de données MongoDB');
 });
 
+// Configuration de l'application Express
 const app = express();
 app.use(bodyParser.json());
-
 app.use(cors());
-
 app.use('/api', routes);
 
+// Gestion des routes non trouvées
 app.use((req, res, next) => {
   res.status(404).json({ message: 'Route non trouvée' });
 });
 
+// Gestion des erreurs internes du serveur
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).json({ message: 'Erreur interne du serveur' });
 });
 
+// Démarrage du serveur
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
   console.log(`Serveur démarré sur le port ${port}`);
