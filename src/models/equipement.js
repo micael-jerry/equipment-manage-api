0a59c3b8c2f2be89cd4402619ff1cd5d9f348e2f
Micael Jerry <hei.jerry.2@gmail.com>
2024-05-25 15:54:59 +0300

    fix: update equipement schema

--- src/models/equipement.js
+++ src/models/equipement.js
@@ -1,29 +1,20 @@
 const mongoose = require('mongoose');
+const { EquipementTypeEnum, EquipementStatusEnum } = require('./equipement.type');
 
 const equipementSchema = new mongoose.Schema({
 	nom: { type: String, required: true },
+	numero_de_serie: { type: String, required: true, unique: true, trim: true },
 	description: { type: String, required: true },
 	pays_d_origine: { type: String, required: true },
 	annee_de_fabrication: { type: Number, required: true },
 	type: {
 		type: String,
-		enum: [
-			'Véhicules terrestres',
-			'Armement',
-			'Matériel de communication',
-			'Équipement médical',
-			'Équipement de protection',
-			'Équipement logistique',
-			'Matériel de génie',
-			'Équipement de surveillance et de reconnaissance',
-			'Équipement de soutien',
-			'Équipement informatique et électronique',
-		],
+		enum: Object.values(EquipementTypeEnum),
 		required: true,
 	},
-	status_equipement: {
+	status: {
 		type: String,
-		enum: ['en cours de maintenance', 'actif', 'inactif'],
+		enum: Object.values(EquipementStatusEnum),
 		required: true,
 	},
 });
