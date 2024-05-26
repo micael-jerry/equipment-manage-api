9c0fcf74c799daabe0ebd98f5f3591e8edf43560
Micael Jerry <hei.jerry.2@gmail.com>
2024-05-26 18:30:33 +0300

    fix: admin access

--- src/routes/equipement.routes.js
+++ src/routes/equipement.routes.js
@@ -6,7 +6,7 @@ const { verifyAuth, verifyAdminAccess } = require('../middleware/auth.middleware
 router.get('/', verifyAuth, equipementController.getEquipements);
 router.post('/', verifyAuth, equipementController.createEquipement);
 router.get('/:id', verifyAuth, equipementController.getEquipementById);
-router.put('/:id', verifyAuth, verifyAdminAccess, equipementController.updateEquipement);
+router.put('/:id', verifyAuth, equipementController.updateEquipement);
 router.delete('/:id', verifyAuth, verifyAdminAccess, equipementController.deleteEquipement);
 
 module.exports = router;
